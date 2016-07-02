c5ca2c15e22123fc2e5c7283e0421f7f8d6b0244
Enya <euvgru@gmail.com>
2016-07-02 22:07:52 +0300

    Added find functionality

--- desktop/src/main/java/com/enya/test/desktop/Main.java
+++ desktop/src/main/java/com/enya/test/desktop/Main.java
@@ -8,14 +8,17 @@ import java.util.UUID;
 
 public class Main {
 
+    private static final EntityManager em = Persistence.createEntityManagerFactory("derby").createEntityManager();
 
     public static void main(String[] args) {
-        EntityManager em = Persistence.createEntityManagerFactory("derby").createEntityManager();
         Scanner s = new Scanner(System.in);
+        System.out.println("enter operation");
         switch (s.next()) {
             case "add": {
                 Employee e = new Employee();
-                String name = args.length > 1 ? args[1] : UUID.randomUUID().toString();
+                System.out.println("enter user name");
+                String inputName = s.next();
+                String name = inputName != null ? inputName : UUID.randomUUID().toString();
                 e.setName(name);
                 em.getTransaction().begin();
                 em.persist(e);
@@ -23,12 +26,18 @@ public class Main {
                 System.out.println("Added ");
                 break;
             }
-            case "all":{
-                List<Employee> all =em.createQuery("SELECT e FROM Employee e").getResultList();
+            case "all": {
+                List<Employee> all = em.createQuery("SELECT e FROM Employee e").getResultList();
                 for (Employee employee : all) {
                     System.out.println(employee);
                 }
                 break;
+            }
+            case "find": {
+                List<Employee> all = em.createQuery("select e from Employee e where e.name like (:name)").setParameter("name", "%" + s.next() + "%").getResultList();
+                for (Employee employee : all) {
+                    System.out.println(employee);
+                }
             }
 
         }
